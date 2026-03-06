0a83f8de59fcfa25249e25de83fbafa1de5285aa
keykey_create <keykeyb29@gmail.com>
2026-03-06 14:48:29 -0700

    finished week 1 exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,88 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int percent = int.Parse(grade);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            if (percent > 93)
+            {
+                letter = "A";
+            }
+            else
+            {
+                letter = "A-";
+            }
+        }
+
+        else if (percent >= 80)
+        {
+            if (percent >= 87)
+            {
+                letter = "B+";
+            }
+            else if (percent > 83)
+            {
+                letter = "B";
+            }
+            else
+            {
+                letter = "B-";
+            }
+        }
+
+        else if (percent >= 70)
+        {
+            if (percent >= 77)
+            {
+                letter = "C+";
+            }
+            else if (percent > 73)
+            {
+                letter = "C";
+            }
+            else
+            {
+                letter = "C-";
+            }
+        }
+
+        else if (percent >= 60)
+        {
+            if (percent >= 67)
+            {
+                letter = "D+";
+            }
+            else if (percent > 63)
+            {
+                letter = "D";
+            }
+            else
+            {
+                letter = "D-";
+            }
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is : {letter}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congrats! You passed!");
+        }
+
+        else
+        {
+            Console.WriteLine("You did not pass, try again, you got this!");
+        }
+
     }
 }
