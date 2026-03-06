f7dc6594ef52bb9b939a9651f529f53fa8d1b298
keykey_create <keykeyb29@gmail.com>
2026-03-06 15:03:15 -0700

    finished week 1 activity 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicnumber = randomGenerator.Next(1,101);
+
+        int guess = -1;
+        int amountguess = 0;
+
+        while (guess != magicnumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicnumber > guess)
+            {
+                amountguess = amountguess + 1;
+                Console.WriteLine("Higher");
+            }
+            else if (magicnumber < guess)
+            {
+                amountguess = amountguess + 1;
+                Console.WriteLine("Lower");                
+            }
+            else
+            {
+                amountguess = amountguess + 1;
+                Console.WriteLine($"Congrats, you guessed the number. It took you {amountguess} guesses");
+            }
+        }
     }
 }
